fadf63341bcf56f5513595f69a7411a95c40d411
uzairaslam <uzairaslam64@gmail.com>
2018-04-14 18:44:01 +0500

    Database Context and Initializer Classes

---
+++ SportsStoreWebApi/Models/ProductDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SportsStoreWebApi.Models
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext() : base("SportsStoreWebApiDb")
+        {
+            Database.SetInitializer<ProductDbContext>(new ProductDbInitializer());
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
+    }
+}
