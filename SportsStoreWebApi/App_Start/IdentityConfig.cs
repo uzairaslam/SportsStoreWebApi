fc379a82f450bf4a82a8710dcef1b7a4e524310a
uzairaslam <uzairaslam64@gmail.com>
2018-04-14 10:11:29 +0500

    Creating the OWIN Startup Class

---
+++ SportsStoreWebApi/App_Start/IdentityConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SportsStoreWebApi.IdentityConfig))]
+namespace SportsStoreWebApi
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app) { }
+    }
+}
--- SportsStoreWebApi/App_Start/RouteConfig.cs
+++ SportsStoreWebApi/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SportsStoreWebApi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Prep", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
