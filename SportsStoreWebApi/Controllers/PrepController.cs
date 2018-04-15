50e4925e189b266b80723184550b367b1dafa515
uzairaslam <uzairaslam64@gmail.com>
2018-04-15 20:07:33 +0500

    Adding Repository

--- SportsStoreWebApi/Controllers/PrepController.cs
+++ SportsStoreWebApi/Controllers/PrepController.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using SportsStoreWebApi.Models;
+using System.Data.Entity;
 
 namespace SportsStoreWebApi.Controllers
 {
     public class PrepController : Controller
     {
+        private IRepository _repo;
+
+        public PrepController()
+        {
+            _repo = new ProductRepository();
+        }
         // GET: Prep
         public ActionResult Index()
         {
-            return View();
+            return View(_repo.Products);
+        }
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            await _repo.DeleteProductAsync(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> SaveProduct(Product product)
+        {
+            await _repo.SaveProductAsync(product);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Orders()
+        {
+            return View(_repo.Orders);
+        }
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            await _repo.DeleteOrderAsync(id);
+            return RedirectToAction("Orders");
+        }
+
+        public async Task<ActionResult> SaveOrder(Order order)
+        {
+            await _repo.SaveOrderAsync(order);
+            return RedirectToAction("Orders");
         }
     }
 }
---
+++ SportsStoreWebApi/Models/ProductRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SportsStoreWebApi.Models
+{
+    public class ProductRepository : IRepository
+    {
+        private readonly ProductDbContext _db = new ProductDbContext();
+
+        public IEnumerable<Product> Products => _db.Products;
+
+        public async Task<int> SaveProductAsync(Product product)
+        {
+            if (product.Id == 0)
+            {
+                _db.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = _db.Products.Find(product.Id);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Category = product.Category;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                }
+            }
+            return await _db.SaveChangesAsync();
+        }
+
+        public async Task<Product> DeleteProductAsync(int productId)
+        {
+            Product dbEntry = _db.Products.Find(productId);
+            if (dbEntry != null)
+            {
+                _db.Products.Remove(dbEntry);
+            }
+            await _db.SaveChangesAsync();
+            return dbEntry;
+        }
+
+        public IEnumerable<Order> Orders => _db.Orders;
+        public async Task<int> SaveOrderAsync(Order order)
+        {
+            if (order.Id == 0)
+            {
+                _db.Orders.Add(order);
+            }
+            return await _db.SaveChangesAsync();
+        }
+
+        public async Task<Order> DeleteOrderAsync(int orderId)
+        {
+            Order dbEntry = _db.Orders.Find(orderId);
+            if (dbEntry != null)
+            {
+                _db.Orders.Remove(dbEntry);
+            }
+            await _db.SaveChangesAsync();
+            return dbEntry;
+        }
+    }
+}
